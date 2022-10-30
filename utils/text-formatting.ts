f5e2155af08c671462a4f86fa56f047b523f4740
haikonguyen <haicorp87@gmail.com>
2022-10-30 11:57:21 +0100

    bugfix: fixing undefined issues

--- utils/text-formatting.ts
+++ utils/text-formatting.ts
@@ -4,8 +4,8 @@ export const EuDateFormat = (date: string | undefined) =>
   dayjs(date).format('DD/MM/YYYY');
 
 export const truncateText = (text: string, limit: number) => {
-  if (text.length <= limit) {
+  if (text?.length <= limit) {
     return text;
   }
-  return text.slice(0, limit) + '...';
+  return text?.slice(0, limit) + '...';
 };
